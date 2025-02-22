7185cf7bb05bb6c19061d885cc47f3fa7ac26b07
Nishant kumar <nishantkr2003nna@gmail.com>
2025-02-22 11:51:59 +0530

    Progressing

--- Lecture 4 - Request and Response/app.js
+++ Lecture 4 - Request and Response/app.js
@@ -2,13 +2,36 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method, req.headers);
+
+  if (req.url === "/") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>Complete Coding</title></head>");
+    res.write("<body><h1>Welcome to my server</h1></body>");
+    res.write("</html>");
+    return res.end();
+  } else if (req.url === "/products") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>Complete Coding</title></head>");
+    res.write("<body><h1>Welcome to my products</h1></body>");
+    res.write("</html>");
+    return res.end();
+  }
+
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>Complete Coding</title></head>");
+  res.write("<body><h1>My Name is Nishant Kumar</h1></body>");
+  res.write("</html>");
+  res.end();
 });
 
 /* function requestListener(req, res) {
   console.log(req);
 }
   */
-//http.createServer(requestListener);
+//http.createServer(requestListen er);
 
 const PORT = 3001;
 server.listen(PORT, () => {
